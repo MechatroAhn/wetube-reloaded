f35c9be192e69d6a67bf7efef8a94fb339c6fa52
MechatroAhn <an17171717@gmail.com>
2022-06-01 02:36:08 +0900

    220531

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import express from "express";
+import req from "express/lib/request";
 
 const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) =>{
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+
+const handleHome= (req, res) =>{
+    return res.send("I love middlewares");
+};
+
+
+
+app.get("/",logger, handleHome);
 
 
 
