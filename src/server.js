5901de01fee04d35e7794ceab6ba166d4907ebaf
MechatroAhn <an17171717@gmail.com>
2022-06-02 19:33:54 +0900

    220601

--- src/server.js
+++ src/server.js
@@ -1,23 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 import req from "express/lib/request";
+import res from "express/lib/response";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) =>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-};
+app.use(logger);
 
 
-const handleHome= (req, res) =>{
-    return res.send("I love middlewares");
-};
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
-
-
-app.get("/",logger, handleHome);
 
 
 
