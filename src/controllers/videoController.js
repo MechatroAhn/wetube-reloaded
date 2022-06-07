1e078652850fee93c22abb4960b7fa70151fa0b2
MechatroAhn <an17171717@gmail.com>
2022-06-07 22:54:24 +0900

    220607

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,7 @@
-export const trending = (req, res) => res.send("Home page Videos");
-export const see = (req, res) =>{
-    console.log(req.params)
-    return res.send(`Watch #${req.params.id}`);
-} 
-export const edit =(req, res) => {
-    console.log(req.params);
-    return res.send("Edit");
-}
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const see = (req, res) => res.render("watch");
+export const edit =(req, res) => res.render("edit");
+
 export const search =(req,res) => res.send("Search");
 export const upload =(req, res) => res.send("Upload");
 export const deleteVideo =(req, res) =>{
