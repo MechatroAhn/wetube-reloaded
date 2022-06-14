045026b20767aab7de7d8b5d8b5b489230fc120b
MechatroAhn <an17171717@gmail.com>
2022-06-15 02:28:46 +0900

    220614

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,18 @@
 import Video from "../models/Video";
 
 
-export const home = (req, res) => {
-    Video.find({}, (error, videos)=>{
-        res.render("home", {pageTitle: "Home", videos});
-    });
-    
-}
+// Video.find({}, (error, videos)=>{
+//     if(error){
+//         return res.render("server-error");
+//     }
+//     return res.render("home", {pageTitle: "Home", videos});
+
+//     }); // callback version
+
+export const home = async(req, res) => {
+    const videos = await Video.find({});
+    return res.render("home", {pageTitle: "Home", videos});  
+};
 export const watch = (req, res) => {
     const { id } = req.params;
     return res.render("watch", {pageTitle: `Watching `});
@@ -26,7 +32,18 @@ export const getUpload = (req, res) =>{
     return res.render("upload",{pageTitle: "Upload Video"});
 };
 
-export const postUpload =(req, res)=>{
-    const { title } = res.body;
+export const postUpload = (req, res) => {
+    const { title, description, hashtags } = req.body;
+    const video = new Video({
+        title: title,
+        description: description,
+        createdAt: Date.now(),
+        hashtags: hashtags.split(",").map((word) => `#${word}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+    });
+    console.log(video);
     return res.redirect("/");
 };
