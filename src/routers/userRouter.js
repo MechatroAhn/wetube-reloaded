f8f385c412420ba119971270f6fc704aa0515f52
MechatroAhn <an17171717@gmail.com>
2022-06-21 04:20:37 +0900

    220620

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import {edit, remove, logout, see} from "../controllers/userController"
 const userRouter = express.Router();
 
-
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
