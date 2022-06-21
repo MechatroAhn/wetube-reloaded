fe05fde94e6b0b96773fb35cc398e07472db806c
MechatroAhn <an17171717@gmail.com>
2022-06-22 00:07:41 +0900

    220621

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
-import session from "express-session"
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -20,9 +21,13 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(
     session({
-    secret: "Hello!",
-    resave:true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave:false,
+    saveUninitialized: false,
+    // cookie:{
+    //     maxAge:200000,
+    // },
+    store: MongoStore.create({mongoUrl: process.env.DB_URL}),
     })
 );
 
