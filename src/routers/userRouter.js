160c2257a09ca6ae9af1d590cf94cea42d222157
MechatroAhn <an17171717@gmail.com>
2022-06-28 17:24:52 +0900

    220628

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,11 +9,11 @@ import {getEdit,
         finishKakaoLogin,
         postChangePassword,
         getChangePassword,} from "../controllers/userController"
-import { protectorMiddleware, publicOnlyMiddleware, uploadFiles } from "../middlewares";
+import { avatarUpload, protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(uploadFiles.single("avatar"), postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUpload.single("avatar"), postEdit);
 userRouter.get("/:id([0-9a-f]{24})", see);
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
