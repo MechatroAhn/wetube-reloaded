f12c1d29a303ba786e760bf45282f448dde7b620
MechatroAhn <an17171717@gmail.com>
2022-07-04 06:08:58 +0900

    220703

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -17,29 +17,36 @@ export const home = async(req, res) => {
 
 export const watch = async(req, res) => {
     const { id } = req.params;
-    const video = await Video.findById(id);
-    const owner = await User.findById(video.owner);
+    const video = await Video.findById(id).populate("owner");
     if(!video){
         return res.status(404).render("404",{pageTitle:"Video not found."});
     }
-    return res.render("watch", {pageTitle: video.title, video, owner});
+    return res.render("watch", {pageTitle: video.title, video});
 };
 
 
 export const getEdit = async(req, res) => {
     const { id } = req.params;
+    const {user: {_id}} = req.session;
     const video = await Video.findById(id);
     if(!video){
         return res.status(404).render("404",{pageTitle:"Video not found."});
     }
+    if(String(video.owner) !== String(req.session.user._id)){
+        return res.status(403).redirect("/");
+    }
     return res.render("edit" ,{pageTitle:`Edit: ${video.title} `, video});
 };
 export const postEdit = async(req, res) => {
+    const {user: {_id}} = req.session;
     const { id } = req.params;
     const { title, description, hashtags } = req.body;
     const video = await Video.exists({_id:id});
     if(!video){
         return res.status(404).render("404",{pageTitle:"Video not found."});
+    }
+    if(String(video.owner) !== String(req.session.user._id)){
+        return res.status(403).redirect("/");
     }
     await Video.findByIdAndUpdate(id, {
         title,
@@ -60,13 +67,16 @@ export const postUpload = async(req, res) => {
     const file = req.file;
     const { title, description, hashtags } = req.body;
     try{
-        await Video.create({
+        const newVideo = await Video.create({
             title,
             description,
             fileUrl:file.path,
             hashtags: Video.formatHashtags(hashtags),
             owner: _id,
         });
+        const user = await User.findById(_id);
+        user.videos.push(newVideo._id);
+        user.save();
         return res.redirect("/");
     } catch(error){
         return res.status(400).errorMessagerender("upload",{pageTitle: "Upload Video", errorMessage: error._message});
@@ -77,7 +87,15 @@ export const postUpload = async(req, res) => {
 
 export const deleteVideo = async(req, res) =>{
     const { id } = req.params;
+    const {user: {_id}} = req.session;
+    const video = await Video.findById(id);
+    if(!video){
+        return res.status(404).render("404",{pageTitle:"Video not found."});
+    }
     try{
+        if(String(video.owner) !== String(req.session.user._id)){
+            return res.status(403).redirect("/");
+        }
         await Video.findByIdAndDelete(id);
         return res.redirect("/");
     } catch(error){
